d5fb1f894f19fabaeeb2fc4e2f56c31c26d3b27f
Myo-kun <myo.kun345@gmail.com>
2013-09-18 12:46:59 -0400

    Removed (redundant?) drawTexturedModalRect

--- myokun/lib/gui/Render.java
+++ myokun/lib/gui/Render.java
@@ -12,14 +12,6 @@ import net.minecraft.client.renderer.Tessellator;
 
 public class Render {
 	
-	public void drawTexturedModalRect(int x, int y, int u, int v, int width, int height) {
-        Tessellator TESLR = Tessellator.instance;
-        TESLR.startDrawingQuads();
-        TESLR.addVertexWithUV((double)(x + 0), (double)(y + height), (double)0, (double)((float)(u + 0) * 1), (double)((float)(v + height) * 1));
-        TESLR.addVertexWithUV((double)(x + width), (double)(y + height), (double)0, (double)((float)(u + width) * 1), (double)((float)(v + height) * 1));
-        TESLR.addVertexWithUV((double)(x + width), (double)(y + 0), (double)0, (double)((float)(u + width) * 1), (double)((float)(v + 0) * 1));
-        TESLR.addVertexWithUV((double)(x + 0), (double)(y + 0), (double)0, (double)((float)(u + 0) * 1), (double)((float)(v + 0) * 1));
-        TESLR.draw();
-    }
+	
 	
 }
