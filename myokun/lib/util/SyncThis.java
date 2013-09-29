8255403510076f5f53e4419e1da76a3282b45cd8
Myo-kun <myo.kun345@gmail.com>
2013-09-29 00:28:38 -0500

    Beginning an automatic client-server sync annotation thingy

---
+++ myokun/lib/util/SyncThis.java
@@ -0,0 +1,15 @@
+/**
+ * MyoKunLib
+ *
+ * SyncThis.java
+ *
+ * @author Myo-kun
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+package myokun.lib.util;
+
+public @interface SyncThis {
+	
+	Class type();
+	
+}
---
+++ myokun/lib/util/Synchronizer.java
@@ -0,0 +1,15 @@
+/**
+ * MyoKunLib
+ *
+ * Synchronizer.java
+ *
+ * @author Myo-kun
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+package myokun.lib.util;
+
+public class Synchronizer {
+	
+	
+	
+}
